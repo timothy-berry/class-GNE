02709dbca1f1d34dc78347f544047423443ef918
Kirk Byers <ktbyers@twb-tech.com>
2024-03-07 16:11:31 -0800

    Make some changes to create a larger merge conflict

--- netmiko_conn.py
+++ netmiko_conn.py
@@ -6,6 +6,14 @@ rtr1 = {
     "username": "juniper",
     "password": "juniper123!",
 }
+rtr2 = {
+    "device_type": "juniper_junos",
+    "host": "rtr2.domain.com",
+    "username": "juniper",
+    "password": "juniper123!",
+}
 
-with ConnectHandler(**rtr1) as net_connect:
-    output = net_connect.send_command("show version")
+for device in (rtr1, rtr2):
+    with ConnectHandler(**device) as net_connect:
+        output = net_connect.send_command("show version")
+        print(output)
