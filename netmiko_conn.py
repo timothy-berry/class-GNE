9483380cb1d7aebfbb271c4b0f6b4666436870ea
Kirk Byers <ktbyers@twb-tech.com>
2024-04-02 13:11:17 -0700

    Adding NYC1 site

--- netmiko_conn.py
+++ netmiko_conn.py
@@ -12,8 +12,14 @@ rtr2 = {
     "username": "juniper",
     "password": "juniper123!",
 }
+nyc1 = {
+    "device_type": "cisco_nxos",
+    "host": "rtr1-nyc.domain.com",
+    "username": "cisco",
+    "password": "cisco!123!",
+}
 
-for device in (rtr1, rtr2):
+for device in (rtr1, rtr2, nyc1):
     # Establish Netmiko Connection and execute simple 'show' command
     with ConnectHandler(**device) as net_connect:
         output = net_connect.send_command("show version")
