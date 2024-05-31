18c3f2e850765cd73e5957d20fc17b438c7e9c2c
Kirk Byers <ktbyers@twb-tech.com>
2024-05-31 16:46:22 -0700

    Removing obsolete tests/ files

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-from os import path
-import os
-
-import pytest
-
-from netmiko import ConnectHandler, FileTransfer, InLineTransfer, SSHDetect
-from test_utils import parse_yaml
--- tests/test_suite.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-RETURN_CODE=0
-PYTEST='py.test -s -v -x'
-
-echo "Starting tests...good luck:"
-echo "Some test for IP addresses"
